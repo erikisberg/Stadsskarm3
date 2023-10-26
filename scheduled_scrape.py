12e7d2f635df29a24c7d22cb4c901a071b2f1941
Erik Isberg <60478965+erikisberg@users.noreply.github.com>
2023-10-26 10:59:55 +0200

    Update scheduled_scrape.py

--- scheduled_scrape.py
+++ scheduled_scrape.py
@@ -1,9 +1,12 @@
+print("Script started")
+
 from skraparen import scrape_site
 
 def main():
+    print("Inside main function")
     data = scrape_site()
-    for item in data:
-        print(item)
+    print("Data scraped:", data)
 
 if __name__ == "__main__":
     main()
+    print("Script finished")

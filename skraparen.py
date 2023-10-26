6f9c93e9e83a181a113b12670af83435324ac4ea
Erik Isberg <60478965+erikisberg@users.noreply.github.com>
2023-10-26 13:11:03 +0200

    Update skraparen.py

--- skraparen.py
+++ skraparen.py
@@ -21,28 +21,12 @@ def scrape_site():
         dishes = restaurant.find_all("div", class_="lunch")
 
         for dish in dishes:
-    dish_data = {}
-    dish_name_element = dish.find("span", class_="dish__name")
-    dish_description_element = dish.find("span", class_="dish__bottomRow")
-    
-    # Check if the dish name element exists and get the text
-    if dish_name_element:
-        dish_data["Dish"] = dish_name_element.text.strip()
-    else:
-        dish_data["Dish"] = "No Dish Name Found"
-
-    # Check if the dish description element exists and get the text
-    if dish_description_element:
-        dish_data["Description"] = dish_description_element.text.strip()
-    else:
-        dish_data["Description"] = "No Description Found"
-
-    dish_data["Restaurant"] = restaurant_name
-    restaurants_data.append(dish_data)
-
+            dish_data = {}
+            dish_data["Restaurant"] = restaurant_name
+            dish_data["Dish"] = dish.text.strip().replace("\n", " - ")
+            restaurants_data.append(dish_data)
 
     return restaurants_data
-
 
 @app.route('/get_lunch_data', methods=['GET'])
 def get_lunch_data_endpoint():
@@ -51,5 +35,3 @@ def get_lunch_data_endpoint():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
-
